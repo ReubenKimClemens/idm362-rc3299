b99e5f3d64970bb7917d4fdbfa36d251414ee5cd
ReubenKimClemens <reuben.kim.clemens@gmail.com>
2025-01-14 19:25:49 -0500

    Initial Commit

---
+++ IDM362-rc3299/IDM362_rc3299App.swift
@@ -0,0 +1,17 @@
+//
+//  IDM362_rc3299App.swift
+//  IDM362-rc3299
+//
+//  Created by Reuben Clemens on 1/14/25.
+//
+
+import SwiftUI
+
+@main
+struct IDM362_rc3299App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
