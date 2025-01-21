8f3d10b0d5e4c637c2fd32692e1bc0007147c00e
ReubenKimClemens <reuben.kim.clemens@gmail.com>
2025-01-21 18:39:29 -0500

    Implement text, headshot, and button

---
+++ IDM362-rc3299/headshotImage.swift
@@ -0,0 +1,18 @@
+//
+//  headshotImage.swift
+//  IDM362-rc3299
+//
+//  Created by Reuben Clemens on 1/21/25.
+//
+
+import SwiftUI
+
+struct headshotImage: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    headshotImage()
+}
