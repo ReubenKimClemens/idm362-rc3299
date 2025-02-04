c41c2b82c0261eab4b86d4a5a009f61906bec1af
ReubenKimClemens <reuben.kim.clemens@gmail.com>
2025-02-03 22:10:39 -0500

    Add assets, rework titlescreen and mainview, add in eventab, jointab, resumetab

--- idm362-rc3299/titleScreen.swift
+++ idm362-rc3299/titleScreen.swift
@@ -10,18 +10,35 @@ import SwiftUI
 struct titleScreen: View {
     var body: some View {
         NavigationView {
-            VStack(alignment: .center) {
-                Text("CirclePlans")
+            VStack(alignment: .center, spacing: 21) {
+                Image("idm362-project-icon")
+                    .frame(width: 234, height: 234)
+                    .clipShape(Circle())
+                    .overlay {
+                        Circle().stroke(.white, lineWidth: 4)
+                    }
+                    .shadow(radius: 7)
+                Text("Path Plans")
                     .font(.largeTitle)
-                    .fontWeight(.heavy)
+                    .fontWeight(.black)
+                    .foregroundColor(Color(red: 0.93, green: 0.93, blue: 0.93))
+                //Add buttons for log in and sign in
                 NavigationLink(destination: mainView()) {
-                    Text("Start")
-                        .padding()
-                        .foregroundColor(.white)
-                        .background(Color.blue)
+                    Text("Log In")
+                        .font(.title)
+                        .fontWeight(.bold)
+                        .padding(.horizontal, 20)
+                        .padding(.vertical, 4)
+                        .foregroundColor(.black)
+                        .background(Color.white)
                         .cornerRadius(10)
                 }
             }
+            .padding(.leading, 100)
+            .padding(.trailing, 100)
+            .padding(.top, 290)
+            .padding(.bottom, 271)
+            .background(Color(red: 0.09, green: 0.15, blue: 0.25))
         }
     }
 }
